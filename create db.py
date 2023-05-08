14ae63df4d34db4c1d79fe798b70b74e63dc3980
Ankit Raj <122270909+AnkitRaaj98@users.noreply.github.com>
2023-05-08 10:59:37 -0700

    Add files via upload

---
+++ create db.py
@@ -0,0 +1,9 @@
+import mysql.connector
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="abc",
+  password="password"
+)
+print(mydb)
+mycursor = mydb.cursor()
+mycursor.execute('create DATABASE ineuron')
